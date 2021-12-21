ced77cd9ab80f21a6e579decf02d432820233b88
Sarathy <vssarat@38f9d370e741.ant.amazon.com>
2021-12-21 13:13:15 -0500

    flux.sh

--- flux.sh
+++ flux.sh
@@ -2,9 +2,10 @@
 # Bootstrapping the cluster with Flux
 # The bootstrap process will automatically create a GitRepository custom resource that points to the given repository
 # The GitRepository resource is named after the namespace where Flux GitOps ToolKit is installed. In this case, it is 'flux-system'
+# The bootstrap process will configure the repository with an SSH key for read-only access
 #
-export CLUSTER_NAME=k8s-addon-cluster
-export GITHUB_TOKEN=ghp
+export CLUSTER_NAME=k8s-production-cluster
+export GITHUB_TOKEN=XXXXX
 export GITHUB_USER=vijayansarathy
 kubectl create ns flux-system
 flux bootstrap github \
@@ -15,6 +16,38 @@ flux bootstrap github \
   --branch=main \
   --path=clusters/$CLUSTER_NAME \
   --personal
+
+
+#
+# In order to authenticate with the external provider API such as AWS, the provider controllers need to have access to credentials. 
+# It could be an IAM User for AWS
+# An AWS user with Administrative privileges is needed to enable Crossplane to create the required resources
+# We wil have to first create a configuration file, secrets.conf, with credeantials of an AWS account in the following format.
+#
+# [default]
+# aws_access_key_id = ABCDEFGHIJ0123456789
+# aws_secret_access_key = 000111r0H7yT5nGP5OPFcZJ+
+#
+# Then using this file, a YAML file that defines a Kubernetes Secret is created as follows
+#
+kubectl -n crossplane-system create secret generic aws-credentials --from-file=credentials=./secrets.conf --dry-run=client -o yaml > aws-credentials.yaml
+
+#
+# Next, deploy the Bitnami's Sealed Secrets controller in the 'sealed-secrets' namespace
+# Generate a SealedSecret corresponding to the 'aws-credentials' Secret created above. This is done using the 'kubeseal' CLI utility
+# The file 'aws-credentials-sealed.yaml' resulting from the operation below is what is deployed to the cluster in the GitOps workflow.
+# Push this file to the './deploy/crossplane-composition' directory of the GitHub repo that Flux is pointing to 
+#
+kubeseal --controller-namespace sealed-secrets --format yaml < aws-credentials.yaml > aws-credentials-sealed.yaml
+
+#
+# Extract the master sealing key from the controller.
+# After extracting the master key, the sealed secrets controller may be termintaed.
+# The controller per se will get deployed as part of the GitOps workflow. 
+# But, you must make sure that the sealing master is deployed before so that all SealedSecrets that were created using this master could be unsealed
+#
+kubectl get secret -n sealed-secrets -l sealedsecrets.bitnami.com/sealed-secrets-key -o yaml > sealing-master.key
+
 
 #
 # Create a Kustomization resource under 'cluster/$CLUSTER_NAME' that points to the 'crossplane' directory in the config repo.
